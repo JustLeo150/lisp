9722a28d4847d5d20304ecaa2143f94e1120f2c3
dracos150 <dracos@list.ru>
2019-03-20 22:35:00 +0300

    Update 7.lisp

--- 7.lisp
+++ 7.lisp
@@ -1,13 +1,13 @@
 7. Определите функцию, удаляющую из исходного списка элементы с четными номерами.
-(defun divide (lst)
+(defun delete-evens (lst)
    ((lambda (head tail)
          (if head
-                (cons head (divide (cdr tail)))
+                (cons head (delete-evens (cdr tail)))
          )  
    ) 
     (car lst)
     (cdr lst)
   )
 )
-(print (divide '(1 2 3 4))) ; => 1 3
-(print (divide '(1 2 3 4 1 2 2 4))) ; => 1 3 1 2
+(print (delete-evens '(1 2 3 4))) ; => 1 3
+(print (delete-evens '(1 2 3 4 1 2 2 4))) ; => 1 3 1 2
