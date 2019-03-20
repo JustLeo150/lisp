87ea6a5540f0796caac88bd59674c11d2239bd11
dracos150 <dracos@list.ru>
2019-03-20 23:48:34 +0300

    Create 21.lisp

---
+++ 21.lisp
@@ -0,0 +1,17 @@
+21. Определите функцию, удаляющую из списка первое вхождение данного элемента на верхнем уровне.
+(defun Del (mainList forDelete)
+   ((lambda (head tail) 
+      (cond 
+         ((null mainList) nil)
+         ((equal head forDelete) tail)
+         (t (cons head (Del tail forDelete)))
+      )
+    )
+    (car mainList)
+    (cdr mainList)
+   )
+)
+(print( Del '((1 2 3) 4 5 (1 2 3) 6)  '(1 2 3))) ; ==> (4 5 (1 2 3) 6)
+(print( Del '((1 2 ) 4 5 (1 2 3) 6)  '(1 2 3))) ; ==> ((1 2) 4 5 6) 
+
+
