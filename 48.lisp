6ebd9bcdff1c833abaa08f46ff4676f6371acd44
dracos150 <dracos@list.ru>
2019-03-21 01:06:31 +0300

    Create 48.lisp

---
+++ 48.lisp
@@ -0,0 +1,16 @@
+48. Напишите предикат (ИМЕЕТ-СВОЙСТВО символ свойство), который проверяет, обладает ли символ данным свойством.
+
+(defun HasProperty(x prop)
+  (FindProperty prop (symbol-plist x))
+)
+
+(defun FindProperty(property list)
+    (cond
+       ((null list) nil)
+       ((equal prop (car list)) T)
+       (t (FindProperty prop (cddr list)))
+    )
+)
+(print(HasProperty 'Symbol 'first)) ; => T
+(print(HasProperty 'Symbol 'second)) ; => T
+(print(HasProperty 'Symbol 'rhird)) ; => NIL
