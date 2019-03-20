f5bbe691dc7af3544ea35a883485d4e8b24dd7da
dracos150 <dracos@list.ru>
2019-03-20 23:34:48 +0300

    Create 16.lisp

---
+++ 16.lisp
@@ -0,0 +1,12 @@
+16. Определите функцию, добавляющую элементы одного списка во второй список, начиная с заданной позиции.
+(defun AddInList (list additionalList index) 
+    ((lambda (head tail) 
+        (cond ((equal 0 index) (cons additionalList list))
+        (t (cons head (AddInList tail additionalList (- index 1))))
+        )
+     )
+     (car list)
+     (cdr list)
+    )
+)
+(print(AddInList '(1 2 3 4 5 7 8 9) "I'm Here" 4)) ; => (1 2 3 4 "I'm Here" 5 7 8 9) 
