1acded8c85ab1fe2831abe9719bb7f1a825088df
dracos150 <dracos@list.ru>
2019-03-20 23:59:51 +0300

    Update 25.lisp

--- 25.lisp
+++ 25.lisp
@@ -1,16 +1,16 @@
 25. Определите функцию, удаляющую из списка каждый четный элемент.
-(defun divide (list)
+(defun deleteOdds (list)
     ((lambda (head tail) 
           (cond 
               ((null tail) list)
-              ((evenp head) (divide tail))
-              (t (cons head (divide tail)))
+              ((evenp head) (deleteOdds tail))
+              (t (cons head (deleteOdds tail)))
           )
      )
      (car list)
      (cdr list)
     )
 )
-(print( divide '(1 2 3 4 5 9 8 7 6 ))) => (1 3 5 9 7 6) 
+(print( deleteOdds '(1 2 3 4 5 9 8 7 6 ))) => (1 3 5 9 7 6) 
              
              
