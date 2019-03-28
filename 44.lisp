a0c63e07a3a8cc96d2f20c53113bb72969ee97cb
dracos150 <dracos@list.ru>
2019-03-28 09:52:00 +0200

    Update 44.lisp

--- 44.lisp
+++ 44.lisp
@@ -1,12 +1,35 @@
-(defun maxi (tree &optional (y 0))
+;42. Определите функцию, находящую максимальное из значений, находящихся в вершинах дерева.
+(defun max2 (el1 el2)
+    (cond
+        ((null el1) el2)
+        ((null el2) el1)
+        ((> el1 el2) el1)
+        (T el2)
+    )
+)
+(defun max-all (arr)
+    (print arr)
+    (cond 
+        ((null arr) nil)
+        (T (max2 (car arr) (max-all (cdr arr) )))
+    )
+)
 
-  (if (null tree) 0)
-     ((< y (car tree)) (setq y (car tree)))
-        (t  (maxi (cadr tree)) (maxi (caddr tree)))))
- 
+( defun max-in-tree (arr)
+    (cond
+        ((null arr) nil)
+        ((null (cdr arr)) (car arr))
+        (T  
+            (max-all 
+                (list
+                    (car arr) 
+                    (max-in-tree (cadr arr)) 
+                    (max-in-tree (caddr arr))
+                )
+            )
+        )
+    )
+)
 
- 
-(print(maxi'(3 (5 (1 nil nil) (7 nil nil)) (4 nil (10 nil nil)))))
- 
-
- 
+(print(max-in-tree '(3 (18 (1 nil nil) (7 nil nil)) (4 nil (15 nil nil))))) ; => 18
+(print(max-in-tree '(3 (5 (1 nil nil) (7 nil nil)) (4 nil (25 nil nil))))) ; => 25
