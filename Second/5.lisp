e73846ef52f630d8f2ab1db11f8257bc25d76560
dracos150 <dracos@list.ru>
2019-04-24 21:48:16 +0300

    Update 5.lisp

--- Second/5.lisp
+++ Second/5.lisp
@@ -3,20 +3,21 @@
 ;нен хотя бы для одного элемента списка список.
 
 (defun somme (pred list-numbers) 
-     (mapcan #'(lambda (number)
+     (not
+       (null
+         (mapcan #'(lambda (number)
             (cond
-                ((not (funcall pred number)) NIL)
-                (t (list t))
+                ( (funcall pred number) (list T))
+                (t NIL)
             )
            ) list-numbers)
-    
+    ))
 )
 
-(defun del-duplicates (l)
-    (cond
-        ((null l) nil)
-        ((member (car l) (cdr l)) (del-duplicates (cdr l)))
-        (t (cons (car l) (del-duplicates (cdr l))))))
- 
 
-(print(del-duplicates(somme 'evenp '(1 2 2 3 3 3 3 3 ))))
+
+(print(somme 'evenp '(1 2 2 ))) ; T 
+
+(print(somme 'evenp '(1 3 5 ))) ; NIL 
+
+(print(somme 'evenp '(2 1 1 ))) ; T 
