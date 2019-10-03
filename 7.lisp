cffe0cf0a2f04aa4b48acf528b9d60fe3a9dc06a
dracos150 <dracos@list.ru>
2019-10-04 01:20:18 +0300

    Add files via upload

--- 7.lisp
+++ 7.lisp
@@ -1,13 +1,1 @@
-;7. Определите функцию, удаляющую из исходного списка элементы с четными номерами.
-(defun delete-evens (lst)
-   ((lambda (head tail)
-         (if head
-                (cons head (delete-evens (cdr tail)))
-         )  
-   ) 
-    (car lst)
-    (cdr lst)
-  )
-)
-(print (delete-evens '(1 2 3 4))) ; => 1 3
-(print (delete-evens '(1 2 3 4 1 2 2 4))) ; => 1 3 1 2
+ 
